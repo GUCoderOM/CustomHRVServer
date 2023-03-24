3933be2e14c2372f87593b18b1f578f82a2c22a1
GUCoderOM <2482572a@student.gla.ac.uk>
2023-03-24 16:29:46 +0000

    Final Submission

--- hrv/admin.py
+++ hrv/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from hrv.models import UserProfile
+from hrv.models import UserProfile,Data
 from django.contrib.auth.models import User
 # Register your models here.
 
@@ -7,3 +7,4 @@ class UserAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('user',)}
     #list_display = ('username', 'email', 'age')
 admin.site.register(UserProfile,UserAdmin)
+admin.site.register(Data)
